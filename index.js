0404830b0ce2c091f24196f8b34f2ecdd13fb23a
freeWillee <will.ik.lee@gmail.com>
2019-02-08 16:28:27 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function getFirstSelector(selector) {
+    return document.querySelector(selector);
+}
+
+function nestedTarget() {
+    return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+    const lis = document.querySelectorAll('.ranked-list li')
+
+    for (let i = 0; i < lis.length; i++) {
+        lis[i].innerHTML + n
+    }
+}
