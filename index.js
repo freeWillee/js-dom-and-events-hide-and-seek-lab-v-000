6aa7bc85ad00110fe1d23bad905a2247beed3911
freeWillee <will.ik.lee@gmail.com>
2019-02-11 12:23:01 -0700

    Done.

--- index.js
+++ index.js
@@ -9,14 +9,28 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-    const lis = document.querySelectorAll('.ranked-list')
+    const rankedLists = document.querySelectorAll('.ranked-list')
 
-    for (let i = 0; i < lis.length; i++) {
-        lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+    for (let i = 0, l = rankedLists.length; i < l; i++) {
+        let children = rankedLists[i].children
+
+        for (let j = 0, k = children.length; j < k; j++) {
+            children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+        }
     }
 }
 
 function deepestChild() {
 
-    return document.querySelector('#grand-node div div div div')
+    // return document.querySelector('#grand-node div div div div')
+
+    let node = document.getElementById('grand-node');
+    let nextNode = node.children[0];
+
+    while (nextNode) {
+        node = nextNode;
+        nextNode = node.children[0];
+    }
+
+    return node;
 }
